815518f6ead4ea847e7ffe0c49955b248d2a3429
benjamin <benjamin.zamour@gmail.com>
2021-08-26 16:37:55 +0200

    src/index: Add index to centralize export for all components

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { default as Dice } from './Dice';
+export { default as DiceSet } from './DiceSet';
+export { default as math } from './math';
+
+export { DiceOptions } from './interfaces/DiceOptions';
+export { DiceRoll } from './interfaces/DiceRoll';
+
+export { DiceSetOptions } from './interfaces/DiceSetOptions';
+export { DiceSetRoll } from './interfaces/DiceSetRoll';
